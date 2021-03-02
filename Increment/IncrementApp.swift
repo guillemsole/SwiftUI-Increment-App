25bee708621a385030751b48ad338e09c2cfe71a
guillemsole <guillemsolecubilo@gmail.com>
2021-03-02 11:54:52 +0100

    Initial Commit

---
+++ Increment/IncrementApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncrementApp.swift
+//  Increment
+//
+//  Created by Guillem Solé Cubilo on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct IncrementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
