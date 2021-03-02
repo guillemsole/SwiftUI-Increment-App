9b60a51e5412697109cd18bf557d0b87a59f18a0
guillemsole <guillemsolecubilo@gmail.com>
2021-03-02 13:27:14 +0100

    CHANGE Create View and Remind View

--- Increment/LandingView.swift
+++ Increment/LandingView.swift
@@ -8,46 +8,53 @@
 import SwiftUI
 
 struct LandingView: View {
+    @State private var isActive: Bool = false
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                Spacer().frame(height: proxy.size.height * 0.18)
-                Text("Increment")
-                    .font(.system(size: 64, weight: .medium))
-                    .foregroundColor(.white)
-                Spacer()
-                Button(action: {}) {
-                    HStack(spacing: 15.0) {
-                        Spacer()
-                        Image(systemName: "plus.circle")
-                        Text("Create a challenge")
-                        Spacer()
+        NavigationView {
+            GeometryReader { proxy in
+                VStack {
+                    Spacer().frame(height: proxy.size.height * 0.18)
+                    Text("Increment")
+                        .font(.system(size: 64, weight: .medium))
+                        .foregroundColor(.white)
+                    Spacer()
+                    NavigationLink(destination: CreateView(), isActive: $isActive) {
+                        Button(action: {
+                            isActive = true
+                        }) {
+                            HStack(spacing: 15.0) {
+                                Spacer()
+                                Image(systemName: "plus.circle")
+                                Text("Create a challenge")
+                                Spacer()
+                            }
+                            .font(.system(size: 24, weight: .semibold))
+                            .foregroundColor(.white)
+                        }
+                        .padding(15.0)
+                        .buttonStyle(PrimaryButtonStyle())
                     }
-                    .font(.system(size: 24, weight: .semibold))
-                    .foregroundColor(.white)
-                }
-                .padding(.horizontal, 15.0)
-                .buttonStyle(PrimaryButtonStyle())
-            }.frame(
-                maxWidth: .infinity,
-                maxHeight: .infinity
-            )
-            .background(
-                Image("pullups")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .overlay(Color.black.opacity(0.4))
-                    .frame(width: proxy.size.width)
-                    .edgesIgnoringSafeArea(.all)
-            )
-        }
+                }.frame(
+                    maxWidth: .infinity,
+                    maxHeight: .infinity
+                )
+                .background(
+                    Image("pullups")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .overlay(Color.black.opacity(0.4))
+                        .frame(width: proxy.size.width)
+                        .edgesIgnoringSafeArea(.all)
+                )
+            }
+        }.accentColor(.primary)
     }
 }
 
 struct LandingView_Previews: PreviewProvider {
     static var previews: some View {
-        LandingView().previewDevice("iPhone 8")
+        // LandingView().previewDevice("iPhone 8")
         LandingView().previewDevice("iPhone 11 Pro")
-        LandingView().previewDevice("iPhone 11 Pro Max")
+        // LandingView().previewDevice("iPhone 11 Pro Max")
     }
 }
---
+++ Increment/RemindView.swift
@@ -0,0 +1,39 @@
+//
+//  RemindView.swift
+//  Increment
+//
+//  Created by Guillem Solé Cubilo on 2/3/21.
+//
+
+import SwiftUI
+
+struct RemindView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            DropdownView()
+            Spacer()
+            Button(action: {}) {
+                Text("Create")
+                    .font(.system(size: 24, weight: .medium))
+                    .foregroundColor(.primary)
+            }
+            .padding(15)
+            Button(action: {}) {
+                Text("Skip")
+                    .font(.system(size: 24, weight: .medium))
+                    .foregroundColor(.primary)
+            }
+        }
+        .navigationBarTitle("Remind")
+        .padding(.bottom, 15)
+    }
+}
+
+struct RemindView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            RemindView()
+        }
+    }
+}
